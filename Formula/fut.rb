e5635dab398fee7f8a341da27d47f5a1b38a55b9
Piotr Fusik <piotr@fusion-lang.org>
2026-07-14 12:09:26 +0200

    Release 3.3.6.

--- Formula/fut.rb
+++ Formula/fut.rb
@@ -1,8 +1,8 @@
 class Fut < Formula
   desc "Fusion Transpiler"
   homepage "https://github.com/fusionlanguage/fut"
-  url "https://github.com/fusionlanguage/fut/archive/refs/tags/fut-3.3.5.tar.gz"
-  sha256 "db7c1b9ce76d28e5c0cad1de4242c58adac9c37d3350af43808772a2eb91a6eb"
+  url "https://github.com/fusionlanguage/fut/archive/refs/tags/fut-3.3.6.tar.gz"
+  sha256 "c091b91d555e8e82c9e55c0b8dbbcb4e38e445573a369d3843e390a964d53bcb"
   license "GPL-3.0-or-later"
   head "https://github.com/fusionlanguage/fut.git", branch: "master"
 
